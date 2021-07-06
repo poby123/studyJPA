4684e6c14d872bf0fae49e7f70a9f89a20687752
poby123 <youngsm12@naver.com>
2021-07-06 22:35:49 +0900

    Part2. Member rest api

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -29,22 +29,28 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
-        if(!findMembers.isEmpty()){
+        if (!findMembers.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
 
     /*
-    * 회원 전체 조회
-    * */
-    public List<Member> findMembers(){
+     * 회원 전체 조회
+     * */
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
     /*
-    * 회원 한 명 조회
-    * */
-    public Member findOne(Long memberId){
+     * 회원 한 명 조회
+     * */
+    public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
+    }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
     }
 }
