e9527f63ea14a89640b43e31e16e3dc960d51689
poby123 <youngsm12@naver.com>
2021-12-02 14:17:09 +0000

    vscode settings

--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JpashopApplicationTests {
 
+	private static 
+
 	@Test
 	void contextLoads() {
+		System.out.println("TEST!!");
+		Assert.assertEquals(1 + 2, 2);
 	}
 
 }
